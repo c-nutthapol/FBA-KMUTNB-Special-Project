0bbf61d523f018208226769e0e45c75a49885a34
PiyawatLoekthaonm <kingdomarm0@gmail.com>
2023-04-10 19:34:50 +0700

    add: Bootstrap Icons

--- resources/views/layouts/partials/css.blade.php
+++ resources/views/layouts/partials/css.blade.php
@@ -3,7 +3,7 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@300;400;500;600;700&display=swap"
             rel="stylesheet">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+        <link rel="stylesheet" href="{{ asset('assets/lib/bootstrap-icons/bootstrap-icons.css') }}">
         <!-- Nucleo Icons -->
         <link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
         <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
@@ -11,3 +11,5 @@
         <script src="https://unpkg.com/@popperjs/core@2"></script>
         <!-- Main Styling -->
         <link href="{{ asset('assets/css/argon-dashboard-tailwind.css?v=1.0.1') }}" rel="stylesheet" />
+        {{-- datepicker css --}}
+        {{-- <link rel="stylesheet" href="{{ asset('assets/js/plugins/datepicker/jquery-ui.min.css') }}" /> --}}
