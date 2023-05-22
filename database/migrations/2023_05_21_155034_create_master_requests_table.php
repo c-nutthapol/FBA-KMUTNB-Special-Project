8ed93c8463af0d7be49b0c6ccca774f696a8be12
anutchai <anuschai.bsod@gmail.com>
2023-05-22 14:21:50 +0700

    update : schema

--- database/migrations/2023_05_21_155034_create_master_requests_table.php
+++ database/migrations/2023_05_21_155034_create_master_requests_table.php
@@ -15,11 +15,14 @@ return new class extends Migration
             $table->id();
             $table->string('name')->comment('ประเภท');
             $table->enum('status', ['active', 'inactive'])->default('active')->comment('สถานะ');
+            $table->bigInteger('created_by', 0, 1)->nullable();
+            $table->bigInteger('updated_by', 0, 1)->nullable();
+            $table->bigInteger('deleted_by', 0, 1)->nullable();
             $table->timestamps();
             $table->softDeletes();
-            $table->foreign('created_by')->comment('สร้างโดย user_id')->references('id')->on('users')->onDelete('SET NULL');
-            $table->foreign('updated_by')->comment('แก้ไขโดย user_id')->references('id')->on('users')->onDelete('SET NULL');
-            $table->foreign('deleted_by')->comment('ลบโดย user_id')->references('id')->on('users')->onDelete('SET NULL');
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('SET NULL');
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('SET NULL');
+            $table->foreign('deleted_by')->references('id')->on('users')->onDelete('SET NULL');
         });
     }
 
--- database/migrations/2023_05_21_160915_create_documents_table.php
+++ database/migrations/2023_05_21_160915_create_documents_table.php
@@ -21,14 +21,14 @@ return new class extends Migration
             $table->date('date')->comment('วันที่เผยแพร่')->nullable();
             $table->enum('pin', ['active', 'inactive'])->default('inactive')->comment('ปักหมุด');
             $table->enum('status', ['active', 'inactive'])->default('active')->comment('สถานะ');
-            $table->bigInteger('created_by', 0, 1)->comment('สร้างโดย user_id')->nullable();
-            $table->bigInteger('updated_by', 0, 1)->comment('แก้ไขโดย user_id')->nullable();
-            $table->bigInteger('deleted_by', 0, 1)->comment('ลบโดย user_id')->nullable();
+            $table->bigInteger('created_by', 0, 1)->nullable();
+            $table->bigInteger('updated_by', 0, 1)->nullable();
+            $table->bigInteger('deleted_by', 0, 1)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
             $table->foreign('created_by')->references('id')->on('users')->onDelete('SET NULL');
             $table->foreign('updated_by')->references('id')->on('users')->onDelete('SET NULL');
             $table->foreign('deleted_by')->references('id')->on('users')->onDelete('SET NULL');
-            $table->timestamps();
-            $table->softDeletes();
         });
     }
 
