d7fa6a218a3b1547034fc52aa3648cbc732909ae
Sakchan-MC <sakchan.mc@gmail.com>
2023-05-29 23:53:49 +0700

    Update UserProject.php

--- app/Models/UserProject.php
+++ app/Models/UserProject.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class UserProject extends Model
 {
     use HasFactory;
-    // SoftDeletes;
 
     protected $guarded = [];
     protected $table = "user_project";
@@ -18,25 +17,4 @@ class UserProject extends Model
     {
         return $this->belongsTo(User::class, "user_id", "id");
     }
-    // protected static function boot()
-    // {
-    //     parent::boot();
-
-    //     /* A method that is called when the model is being created. */
-    //     static::creating(function ($model) {
-    //         $model->created_by = auth()->check() ? auth()->user()->id : null;
-    //         $model->updated_by = auth()->check() ? auth()->user()->id : null;
-    //     });
-
-    //     /* A method that is called when the model is being updated. */
-    //     static::updating(function ($model) {
-    //         $model->updated_by = auth()->check() ? auth()->user()->id : null;
-    //     });
-
-    //     /* A method that is called when the model is being deleted. */
-    //     static::deleting(function ($model) {
-    //         $model->deleted_by = auth()->check() ? auth()->user()->id : null;
-    //         $model->save();
-    //     });
-    // }
 }
