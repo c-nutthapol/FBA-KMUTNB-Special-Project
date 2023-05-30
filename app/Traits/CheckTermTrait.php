593d9f14486ee32b2f0c2d62270b8fb88c7cbc5a
Sakchan-MC <sakchan.mc@gmail.com>
2023-05-30 14:17:39 +0700

    แก้หาเทอมไม่เจอ

--- app/Traits/CheckTermTrait.php
+++ app/Traits/CheckTermTrait.php
@@ -7,7 +7,6 @@ use App\Models\ProjectStepConfig;
 
 trait CheckTermTrait
 {
-
     /**
      * ฟังก์ชันนี้จะคืนค่าภาคการศึกษาปัจจุบันและวันที่ในรูปแบบไทย หรือข้อความที่ระบุว่าไม่มีภาคการศึกษาปัจจุบัน
      *
@@ -17,69 +16,73 @@ trait CheckTermTrait
      * จะคืนค่าข้อความว่าไม่มีกำหนดการและวันที่ปัจจุบันในรูปแบบไทย
      */
 
-    public static function term(string $data_format = 'string')
+    public static function term(string $data_format = "string")
     {
-        $dateCurrent = date('Y-m-d');
-        $term = EduTerm::where('start_date', '>=', $dateCurrent)->where('end_date', '<=', $dateCurrent)->first();
-        if ($data_format == 'string') {
+        $dateCurrent = date("Y-m-d");
+        $term = EduTerm::where("start_date", "<=", $dateCurrent)
+            ->where("end_date", ">=", $dateCurrent)
+            ->first();
+        if ($data_format == "string") {
             if ($term) {
-                return 'ภาคเรียนที่ ' . $term->term . 'ปีการศึกษา ' . $term->year . ' วันที่ ' . thaidate();
+                return "ภาคเรียนที่ " . $term->term . "ปีการศึกษา " . $term->year . " วันที่ " . thaidate();
             } else {
-                return 'ยังไม่มีกำหนดการ วันที่ ' . thaidate();
+                return "ยังไม่มีกำหนดการ วันที่ " . thaidate();
             }
-        } elseif ($data_format == 'array') {
+        } elseif ($data_format == "array") {
             if ($term) {
                 return [
-                    'term' => $term->term,
-                    'year' => $term->year,
-                    'current_date' => $dateCurrent
+                    "term" => $term->term,
+                    "year" => $term->year,
+                    "current_date" => $dateCurrent,
                 ];
             }
-        } elseif ($data_format == 'object') {
+        } elseif ($data_format == "object") {
             if ($term) {
                 return (object) [
-                    'term' => $term->term,
-                    'year' => $term->year,
-                    'current_date' => $dateCurrent
+                    "term" => $term->term,
+                    "year" => $term->year,
+                    "current_date" => $dateCurrent,
                 ];
             }
         }
     }
 
-
     /**  ฟังก์ชัน `step()` โดยรับพารามิเตอร์สองตัว: `step` ซึ่งเป็นจำนวนเต็มและมีค่าเริ่มต้นเป็น 1 - 5
      * และ `data_format` ซึ่งจะรับค่าเป็นสตริงและมีค่าเริ่มต้นเป็น 'string' จะแสดงผลลัพธ์ ได้ 3 แบบ คือ string, array, object
      */
 
-    public static function step(int $step = 1, string $data_format = 'string')
+    public static function step(int $step = 1, string $data_format = "string")
     {
-        $dateCurrent = date('Y-m-d');
+        $dateCurrent = date("Y-m-d");
         if ($step >= 1 && $step <= 5) {
-            $get_project_step = ProjectStepConfig::select('phase_' . $step . '_start_date as start_date', 'phase_' . $step . '_end_date as end_date')
-                ->whereDate('phase_' . $step . '_start_date', '>=', $dateCurrent)
-                ->whereDate('phase_' . $step . '_end_date', '<=', $dateCurrent)
-                ->where('phase_' . $step . '_status', 1)
+            $get_project_step = ProjectStepConfig::select(
+                "phase_" . $step . "_start_date as start_date",
+                "phase_" . $step . "_end_date as end_date"
+            )
+                ->whereDate("phase_" . $step . "_start_date", ">=", $dateCurrent)
+                ->whereDate("phase_" . $step . "_end_date", "<=", $dateCurrent)
+                ->where("phase_" . $step . "_status", 1)
                 ->first(); // เพิ่มเมธอด first() เพื่อดึงข้อมูลแถวแรกที่เจอ
-            if ($data_format == 'string') {
+            if ($data_format == "string") {
                 if ($get_project_step) {
-                    return 'ก่อน ' . $get_project_step->end_date->thaidate('j M y');
+                    return "ก่อน " . $get_project_step->end_date->thaidate("j M y");
                 } else {
-                    return 'รอกำหนดการ';
+                    return "รอกำหนดการ";
                 }
-            } elseif ($data_format == 'array') {
+            } elseif ($data_format == "array") {
                 if ($get_project_step) {
                     return [
-                        'start_date' => $get_project_step->start_date->toDateString(),
-                        'end_date' => $get_project_step->end_date->toDateString(),
-                        'current_date' => $dateCurrent,
+                        "start_date" => $get_project_step->start_date->toDateString(),
+                        "end_date" => $get_project_step->end_date->toDateString(),
+                        "current_date" => $dateCurrent,
                     ];
                 }
-            } elseif ($data_format == 'object') {
+            } elseif ($data_format == "object") {
                 if ($get_project_step) {
                     return (object) [
-                        'start_date' => $get_project_step->start_date->toDateString(),
-                        'end_date' => $get_project_step->end_date->toDateString(),
-                        'current_date' => $dateCurrent,
+                        "start_date" => $get_project_step->start_date->toDateString(),
+                        "end_date" => $get_project_step->end_date->toDateString(),
+                        "current_date" => $dateCurrent,
                     ];
                 }
             } else {
