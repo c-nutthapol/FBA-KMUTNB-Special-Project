f7e841880b78940c67d1987232d4c1009728c1c4
Sakchan-MC <sakchan.mc@gmail.com>
2023-06-18 01:57:16 +0700

    ้เพิ่ม ประวัติการแนบไฟล์

---
+++ app/Http/Livewire/Students/File.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Livewire\Students;
+
+use App\Models\File as Files;
+use Auth;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Collection;
+use Livewire\Component;
+
+class File extends Component
+{
+    public function render(): View|\Illuminate\Foundation\Application|Factory|Application
+    {
+        return view("livewire.students.file");
+    }
+
+    public function getRequestProperty(): Collection
+    {
+        return Files::where("project_id", "=", Auth::user()->projects->first()->id)->orderBy("id", "desc")->get();
+    }
+}
