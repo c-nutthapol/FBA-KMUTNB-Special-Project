d69ec0a7e5b1a12f310fbf2b437c20d1736c6112
anutchai <anuschai.bsod@gmail.com>
2023-06-27 16:36:22 +0700

    update : ปิด vite

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -8,7 +8,7 @@
 
     <!-- CSS -->
     @include('layouts.partials.css')
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     @livewireStyles
 </head>
 
