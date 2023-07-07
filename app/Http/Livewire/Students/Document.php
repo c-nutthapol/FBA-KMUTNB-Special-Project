db4147fb98260d42e02f1716f9d6128129227769
anutchai <anuschai.bsod@gmail.com>
2023-07-07 14:43:58 +0700

    update : เอกสารดาวน์โหลด

--- app/Http/Livewire/Students/Document.php
+++ app/Http/Livewire/Students/Document.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Livewire\Students;
 
+use App\Models\Document as ModelsDocument;
 use Livewire\Component;
 
 class Document extends Component
 {
     public function render()
     {
-        return view('livewire.students.document');
+        $data = ModelsDocument::query()
+        ->where('status', 'active')
+        ->orderBy("pin")
+        ->orderBy("id", "asc")
+        ->paginate(10);
+        return view('livewire.students.document')->with(compact('data'));
     }
+
+
 }
